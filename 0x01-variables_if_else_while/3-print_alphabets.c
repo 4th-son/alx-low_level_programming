26daa1755c89587db5393eabde2ccc8a7c028db3
michael ogozi <michealogozi.com>
2023-03-03 15:11:52 -0800

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - main block
@@ -13,20 +12,19 @@ int main(void)
 {
 
 	char c = 'a';
-
 	while (c <= 'z')
-
 	{
 		putchar(c);
 		c++;
 	}
-	 c = 'A';
+
+	c = 'A';
 
 	while (c <= 'Z')
 
 		putchar (c);
 		c++;
 
-		putchar ('\n')
-		return (0);
+	putchar ('\n');
+	return (0);
 }
